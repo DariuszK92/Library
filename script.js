e691d3cdfc6f11f76e302115756a446276a0c8ed
DariuszK92 <60391491+DariuszK92@users.noreply.github.com>
2023-03-10 19:43:07 +0100

    Add JS

--- script.js
+++ script.js
@@ -1,16 +1,61 @@
+const LIBRARY = document.querySelector('.library');
+
+const myLibrary = [
+    {
+        title: 'The Hobbit',
+        author: 'J.R.R.Tolkien',
+        pages: '324',
+        read: 'rear'
+    },
+    {
+        title: 'Andre',
+        author: 'J.R.R.Tolkien',
+        pages: '324',
+        read: 'rear'
+    },
+    {
+        title: 'Andre',
+        author: 'J.R.R.Tolkien',
+        pages: '324',
+        read: 'rear'
+    },
+    {
+        title: 'Andre',
+        author: 'J.R.R.Tolkien',
+        pages: '324',
+        read: 'rear'
+    },
+    {
+        title: 'Andre',
+        author: 'J.R.R.Tolkien',
+        pages: '324',
+        read: 'rear'
+    },
+    {
+        title: 'Andre',
+        author: 'J.R.R.Tolkien',
+        pages: '324',
+        read: 'rear'
+    }
+];
+
 function Book(title, author, pages, read) {
-  (this.title = title),
-    (this.author = author),
-    (this.pages = pages),
-    (this.read = read);
+    // eslint-disable-next-line no-unused-expressions, no-sequences
+    (this.title = title), (this.author = author), (this.pages = pages), (this.read = read);
 }
 
 Book.prototype.info = function () {
-  console.log(
-    `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
-  );
+    console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`);
 };
 
-const hobbit = new Book("The Hobbit", "J.R.R.Tolkien", "324", "read");
+const hobbit = new Book('The Hobbit', 'J.R.R.Tolkien', '324', 'read');
 
-hobbit.info();
+function addBookToLibrary() {
+    // do stuff here
+}
+
+myLibrary.forEach((book) => {
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('book');
+    LIBRARY.appendChild(newDiv);
+});
